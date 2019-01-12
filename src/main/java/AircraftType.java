31285f1b0cd149ef3a6259fea495ca7cac08f85f
Steven Kay <stevendkay@gmail.com>
2019-01-12 19:57:58 +0000

    added Aircraft type enum and tests

---
+++ src/main/java/AircraftType.java
@@ -0,0 +1,17 @@
+public enum AircraftType {
+
+	BOEING_747(450),
+	AIRBUS_A320(236)
+	;
+
+
+	private final int capacity;
+
+	AircraftType(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public int getCapacity() {
+		return this.capacity;
+	}
+}
---
+++ src/test/java/TestAircraftType.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestAircraftType {
+
+	private AircraftType type;
+
+	@Before
+	public void setUp() {
+		type = AircraftType.BOEING_747;
+	}
+
+	@Test
+	public void hasCapacity() {
+		System.out.println(type);
+		assertEquals(450, type.getCapacity());
+	}
+}
